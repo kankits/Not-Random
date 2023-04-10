4d086ea05deabb94fa4a31cbea4f8fb3d4645da6
Rayyan <rayyanatdps@gmail.com>
2023-04-10 05:42:17 +0530

    csv files and loading script

---
+++ load/create_table.sql
@@ -0,0 +1,147 @@
+DROP TABLE IF EXISTS raw_cities;
+DROP TABLE IF EXISTS Raw_Places;
+DROP TABLE IF EXISTS Raw_Hotels;
+DROP TABLE IF EXISTS raw_restaurants;
+DROP TABLE IF EXISTS raw_flights;
+DROP TABLE IF EXISTS raw_trains;
+DROP TABLE IF EXISTS raw_stations;
+
+CREATE TABLE raw_cities (
+    CityName varchar NOT NULL,
+    State varchar NOT NULL
+);
+
+CREATE TABLE raw_places (
+    CityName varchar NOT NULL,
+    Place varchar NOT NULL,
+    Rating int NOT NULL
+);
+
+CREATE TABLE raw_hotels (
+    CityName varchar NOT NULL,
+    HotelName varchar NOT NULL,
+    RoomRent int NOT NULL,
+    StarRating decimal NOT NULL,
+    HotelAddress varchar NOT NULL,
+    HotelPincode varchar NOT NULL,
+    HotelDescription varchar NOT NULL,
+    FreeWifi boolean NOT NULL,
+    FreeBreakfast boolean NOT NULL,
+    HasSwimmingPool boolean NOT NULL
+);
+
+CREATE TABLE raw_restaurants(
+    Name varchar NOT NULL,
+    Location varchar NOT NULL,
+    Locality varchar NOT NULL,
+    CityName varchar NOT NULL,
+    Cuisine varchar NOT NULL,
+    Rating decimal NOT NULL,
+    Votes int NOT NULL,
+    Cost int NOT NULL
+);
+
+CREATE TABLE raw_flights (
+    flight_number varchar NOT NULL,
+    airline varchar NOT NULL,
+    origin_cityname varchar NOT NULL,
+    destination_cityname varchar NOT NULL
+);
+
+CREATE TABLE raw_trains (
+    train_no varchar NOT NULL,
+    train_name varchar NOT NULL,
+    seq int NOT NULL,
+    station_code varchar NOT NULL, 
+    station_name varchar NOT NULL,
+    source_station_code varchar NOT NULL, 
+    destination_station_code varchar NOT NULL
+);
+
+CREATE TABLE raw_stations (
+    station_code varchar NOT NULL,
+    station_name varchar NOT NULL,
+    CityName varchar NOT NULL
+);
+---------------------------------------------------------------------------
+DROP TABLE IF EXISTS Cities CASCADE;
+DROP TABLE IF EXISTS Hotels CASCADE;
+DROP TABLE IF EXISTS Places CASCADE;
+DROP TABLE IF EXISTS Restaurants CASCADE;
+DROP TABLE IF EXISTS Flights CASCADE;
+DROP TABLE IF EXISTS Stations CASCADE;
+DROP TABLE IF EXISTS TrainPath CASCADE;
+DROP TABLE IF EXISTS TrainInfo CASCADE;
+
+CREATE TABLE Cities (
+    CityId int NOT NULL,
+    CityName varchar NOT NULL,
+    State varchar NOT NULL,
+    PRIMARY KEY (CityId)
+);
+
+CREATE TABLE Hotels (
+    Cityid int NOT NULL REFERENCES Cities(CityId),
+    HotelName varchar NOT NULL,
+    Locality varchar NOT NULL,
+    StarRating decimal NOT NULL,
+    Rent int NOT NULL,
+    FreeBreakfast boolean NOT NULL,
+    FreeWifi boolean  NOT NULL,
+    HotelPincode varchar NOT NULL,
+    HasSwimmingPool boolean NOT NULL,
+    HotelDescription varchar NOT NULL,
+    PRIMARY KEY (cityid, Hotelname, Locality)
+);
+
+CREATE TABLE Places (
+    CityId int NOT NULL REFERENCES Cities(CityId),
+    Place varchar NOT NULL,
+    Rating decimal NOT NULL,
+    num_rating int NOT NULL,
+    PRIMARY KEY (Cityid, Place)
+);
+
+CREATE TABLE Restaurants(
+    CityId int NOT NULL REFERENCES Cities(CityId),
+    Name varchar NOT NULL,
+    Locality varchar NOT NULL,
+    Cost int NOT NULL,
+    Cuisine varchar NOT NULL,
+    Votes int NOT NULL,
+    Rating decimal NOT NULL,
+    PRIMARY KEY (CityId, Name, Locality)
+);
+
+CREATE TABLE Flights (
+    flight_number varchar NOT NULL,
+    airline varchar NOT NULL,
+    source_cityid int NOT NULL REFERENCES Cities(CityId),
+    destination_cityid int NOT NULL REFERENCES Cities(CityId),
+    PRIMARY KEY (flight_number, airline)
+);
+
+CREATE TABLE Stations (
+    CityId int NOT NULL REFERENCES Cities(CityId),
+    station_code varchar NOT NULL,
+    station_name varchar NOT NULL,
+    PRIMARY KEY (station_code)
+);
+
+CREATE TABLE TrainInfo (
+    train_no varchar NOT NULL,
+    train_name varchar NOT NULL,
+    source_station_code varchar NOT NULL REFERENCES Stations(station_code),
+    destination_station_code varchar NOT NULL REFERENCES Stations(station_code),
+    PRIMARY KEY (train_no)
+);
+
+CREATE TABLE TrainPath (
+    train_no varchar NOT NULL REFERENCES TrainInfo(train_no),
+    station_code varchar NOT NULL REFERENCES Stations(station_code),
+    seq int NOT NULL,
+    PRIMARY KEY (train_no, station_code)
+);
+
+
+
---
+++ load/import_database.sql
@@ -0,0 +1,148 @@
+COPY raw_cities from '/app/Not-Random/data/raw_cities.csv' DELIMITER ',' CSV HEADER;
+COPY raw_places from '/app/Not-Random/data/raw_places.csv' DELIMITER ',' CSV HEADER;
+COPY raw_hotels from '/app/Not-Random/data/raw_hotels.csv' DELIMITER ',' CSV HEADER;
+COPY raw_restaurants from '/app/Not-Random/data/raw_restaurants.csv' DELIMITER ',' CSV HEADER;
+COPY raw_flights from '/app/Not-Random/data/raw_flights.csv' DELIMITER ',' CSV HEADER;
+COPY raw_trains from '/app/Not-Random/data/raw_trains.csv' DELIMITER ',' CSV HEADER;
+COPY raw_stations from '/app/Not-Random/data/raw_stations.csv' DELIMITER ',' CSV HEADER;
+
+COPY
+(
+    select row_number() over (order by cityName) as CityId, CityName, State
+    from raw_cities
+)
+to '/app/Not-Random/data/cities.csv'
+csv header;
+
+COPY cities from '/app/Not-Random/data/cities.csv' DELIMITER ',' CSV HEADER;
+
+DROP VIEW IF EXISTS cities_unique;
+CREATE VIEW cities_unique as (select min(cityid) as cityid, cityName from cities group by cityName);
+
+COPY
+(
+    select
+        Cityid,
+        HotelName,
+        min(HotelAddress) as Locality,
+        min(starRating) as starRating,
+        avg(roomrent) :: integer as rent,
+        min(FreeBreakfast::integer)::boolean as FreeBreakfast,
+        min(FreeWifi::integer)::boolean as FreeWifi,
+        min(Hotelpincode) as pincode,
+        min(HasSwimmingPool::integer)::boolean as HasSwimmingPool,
+        min(HotelDescription) as HotelDescription
+
+    from cities_unique, raw_hotels
+    where raw_hotels.CityName = cities_unique.CityName or (raw_hotels.CityName = 'Bangalore' and cities_unique.CityName = 'Bengaluru') 
+    group by cityid, HotelName
+)
+to '/app/Not-Random/data/hotels.csv'
+csv header;
+
+COPY hotels from '/app/Not-Random/data/hotels.csv' DELIMITER ',' CSV HEADER;
+
+
+COPY 
+(
+    select cityid, place, avg(rating) as rating, count(*) as num_rating
+    from Raw_Places, cities_unique
+    where Raw_Places.cityName = cities_unique.cityname or (Raw_Places.CityName = 'Bangalore' and cities_unique.CityName = 'Bengaluru') 
+    group by cityid, place
+)
+to '/app/Not-Random/data/places.csv'
+csv header;
+
+COPY places from '/app/Not-Random/data/places.csv' DELIMITER ',' CSV HEADER;
+
+COPY
+(
+    select
+        cityid,
+        Name,
+        Location as Locality,
+        min(Cost) as Cost,
+        min(Cuisine) as Cuisine,
+        min(Votes) as Votes,
+        min(Rating) as Rating
+    from raw_restaurants, cities_unique
+    where raw_restaurants.cityName = cities_unique.cityname or (raw_restaurants.CityName = 'Bangalore' and cities_unique.CityName = 'Bengaluru') 
+    group by cityid, Name, Location
+)   
+to '/app/Not-Random/data/restaurants.csv'
+csv header;
+
+COPY restaurants from '/app/Not-Random/data/restaurants.csv' DELIMITER ',' CSV HEADER;
+
+COPY
+(
+    select
+        flight_number,
+        airline,
+        min(source_cities.cityid) as source_cityid,
+        min(destination_cities.cityid) as destination_cityid
+    from raw_flights, cities_unique as source_cities, cities_unique as destination_cities
+    where
+        (raw_flights.origin_cityname = source_cities.cityname or (raw_flights.origin_cityname = 'Bangalore' and source_cities.CityName = 'Bengaluru'))
+        and
+        (raw_flights.destination_cityname = destination_cities.cityname or (raw_flights.destination_cityname = 'Bangalore' and destination_cities.CityName = 'Bengaluru'))
+    group by flight_number, airline
+)   
+to '/app/Not-Random/data/flights.csv'
+csv header;
+
+COPY flights from '/app/Not-Random/data/flights.csv' DELIMITER ',' CSV HEADER;
+
+COPY
+(
+    select
+        min(CityId) as CityId,
+        station_code,
+        min(station_name) as station_name
+    from raw_stations, cities_unique
+    where
+        raw_stations.CityName = cities_unique.cityname or (raw_stations.CityName = 'Bangalore' and cities_unique.CityName = 'Bengaluru')
+    group by station_code
+)   
+to '/app/Not-Random/data/stations.csv'
+csv header;
+
+COPY stations from '/app/Not-Random/data/stations.csv' DELIMITER ',' CSV HEADER;
+
+COPY
+(
+    select
+        train_no,
+        min(train_name) as train_name,
+        min(source_station_code) as source_station_code,
+        min(destination_station_code) as destination_station_code
+    from raw_trains
+    where
+        (source_station_code in (select station_code from stations))
+        and
+        (destination_station_code in (select station_code from stations))
+    group by train_no
+)   
+to '/app/Not-Random/data/traininfo.csv'
+csv header;
+
+COPY TrainInfo from '/app/Not-Random/data/traininfo.csv' DELIMITER ',' CSV HEADER;
+
+COPY
+(
+    select
+        train_no,
+        station_code,
+        min(seq) as seq
+    from raw_trains
+    where
+        (train_no in (select train_no from TrainInfo))
+        and
+        (station_code in (select station_code from stations))
+    group by train_no, station_code
+)   
+to '/app/Not-Random/data/trainpath.csv'
+csv header;
+
+COPY TrainPath from '/app/Not-Random/data/trainpath.csv' DELIMITER ',' CSV HEADER;
+
